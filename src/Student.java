9839869bdc1a0ffcba4f27f214ca7c9cc71b2f53
Janidu Rukshan <janidurukshan1111@gmail.com>
2026-06-10 20:27:11 +0530

    Create Student class

---
+++ src/Student.java
@@ -0,0 +1,23 @@
+public class Student {
+    private String studentId;
+    private String studentName;
+    private double marks;
+
+    public Student(String studentId, String studentName, double marks) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+        this.marks = marks;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public double getMarks() {
+        return marks;
+    }
+}
